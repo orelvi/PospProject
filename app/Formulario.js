510ae44c06a58bea5e10d4c3af72df0634439067
Francisco Orelvi Santos <orelvi@gmail.com>
2018-06-12 21:14:24 -0400

    201806122114

--- app/Formulario.js
+++ app/Formulario.js
@@ -1,75 +1,128 @@
 
 
-function limpiarFormulario(inputs){
-    
-    inputs.forEach(function(v,i) {
-        document.getElementById(v).value="";
+function limpiarFormulario(inputs) {
+
+    inputs.forEach(function (v, i) {
+        document.getElementById(v).value = "";
     });
 }
 
-var estudiantes=[];
-var storage = window.localStorage;
- 
- window.onload = function(){
-    if (typeof(storage) !== "undefined") {
-        var localEestudiantes = JSON.parse(storage.getItem("Estudiantes"));
-       agregarEstudiante(localEestudiantes);
+var estudiantes = [];
+var Storage = window.localStorage;
 
+window.onload = function () {
+
+    var dEstudiantes = Storage.getItem("estudiantes");
+
+    if (dEstudiantes !== undefined) {
+        estudiantes = JSON.parse(dEstudiantes);
+        estudiantes.forEach((v, i) => agregarRow(v));
+        console.log("Estudiantes:" + dEstudiantes);
     } else {
-        consol.log("Storage no soportado");
+        console.log("Estudiantes no encontrado");
     }
-    }  
+}
 
 
-function agregarEstudiante(estudiante){
 
-    
+function agregarEstudiante(est) {
 
-        var tablaEstudiante= document.getElementById("tabla_estudiante")
-        var tdNombre = document.createElement("td");
-        var tdMatricula = document.createElement("td");
-        var tdIdentificacion = document.createElement("td");
 
-        var tr= document.createElement("tr");
-        tdNombre.textContent= estudiante.nombre;
-        tdMatricula.textContent= estudiante.matricula;
-        tdIdentificacion.textContent= estudiante.identificacion;
+    estudiantes.push(est);
+    //Agreago a la tabla HTML
+    agregarRow(est);
 
-        tr.appendChild(tdNombre);
-        tr.appendChild(tdMatricula);
-        tr.appendChild(tdIdentificacion);
 
-        tablaEstudiante.appendChild(tr);
+    Storage.setItem("estudiantes", JSON.stringify(estudiantes));
+    //TODO: Guarda en el localStorage
 
-        //guardaEstudiante(estudiante);
+}
+
+function buscarEstudiantePorMatricula(matricula) {
+    var estudiante=null;
+
+    estudiantes.forEach(function (v, i) {
+        if (v.matricula === matricula) {
+            estudiante = v;
+        }
+    });
+
+    return estudiante;
+}
+
+function editarEstudiante(btnEditar) {
+    var matricula = btnEditar.getAttribute("data-matricula");
+    // alert(matricula);
+
+
+    var estudiante = buscarEstudiantePorMatricula(matricula);
+
+   
+    //console.log(estudiante);
+    if (estudiante == null) {
+        console.log("No encontrado");
+        return;
+    }
+
+    document.getElementById("nombre").value = estudiante.nombre;
+    document.getElementById("matricula").value = estudiante.matricula;
+    document.getElementById("identificacion").value = estudiante.identificacion;
+}
+
+function agregarRow(estudiante) {
+
+
+
+    var tablaEstudiante = document.getElementById("tabla_estudiante")
+    var tdNombre = document.createElement("td");
+    var tdMatricula = document.createElement("td");
+    var tdIdentificacion = document.createElement("td");
+    var tdActions = document.createElement("td");
+
+    var btnEditar = document.createElement("button");
+    btnEditar.textContent = "Editar";
+    btnEditar.setAttribute("onclick", "editarEstudiante(this);");
+
+    btnEditar.setAttribute("data-matricula", estudiante.matricula);
+    tdActions.appendChild(btnEditar);
+
+
+    var tr = document.createElement("tr");
+    tdNombre.textContent = estudiante.nombre;
+    tdMatricula.textContent = estudiante.matricula;
+    tdIdentificacion.textContent = estudiante.identificacion;
+
+
+    tr.appendChild(tdNombre);
+    tr.appendChild(tdMatricula);
+    tr.appendChild(tdIdentificacion);
+    tr.appendChild(btnEditar);
+
+    tablaEstudiante.appendChild(tr);
+
+    //guardaEstudiante(estudiante);
 
 
 }
 
 //Leer los inputs y agregarlos a la tabla
-function leerEstudiante(){
-    var nombre =document.getElementById("nombre").value;
-    var matricula =document.getElementById("matricula").value;
-    var identificacion =document.getElementById("identificacion").value;
+function leerEstudiante() {
+    var nombre = document.getElementById("nombre").value;
+    var matricula = document.getElementById("matricula").value;
+    var identificacion = document.getElementById("identificacion").value;
 
     var est = new Estudiante();
-    est.nombre=nombre;
-    est.matricula=matricula;
-    est.identificacion=identificacion;
+    est.nombre = nombre;
+    est.matricula = matricula;
+    est.identificacion = identificacion;
 
-    limpiarFormulario(["nombre","matricula","identificacion"]);
-    estudiantes.push(est);
+    limpiarFormulario(["nombre", "matricula", "identificacion"]);
+
     console.log(est);
     agregarEstudiante(est);
-    guardaEstudiante(est);
-   
+    //guardaEstudiante(est);
+
 
 }
 
-function guardaEstudiante(){
-    
-    
-    storage.setItem("Estudiantes",JSON.stringify(estudiantes));
-    
-    
-}
+
